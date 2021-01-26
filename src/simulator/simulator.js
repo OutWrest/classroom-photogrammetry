1fba9f160b5dd40537a6be03367d1a4c40e1a23b
Salah <47069801+OutWrest@users.noreply.github.com>
2021-01-26 14:10:48 -0600

    simulator init using p5js

---
+++ src/simulator/simulator.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight, WEBGL);
+}
+
+function draw() {
+    background(255);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
